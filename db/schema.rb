95673b332abe87c1def067d8607674b284e515ab
botiuk <svetabotiuk@gmail.com>
2023-10-31 10:13:32 +0200

    add pg to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_24_081050) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.string "country"
     t.string "region"
@@ -26,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_081050) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "location_id", null: false
+    t.bigint "location_id", null: false
     t.index ["location_id"], name: "index_posts_on_location_id"
   end
 
