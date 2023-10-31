4343a87df84ee2e9c8f52153f9c09f150e954e8d
botiuk <svetabotiuk@gmail.com>
2023-10-31 12:01:54 +0200

    add model Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_081050) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_095926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_081050) do
     t.datetime "updated_at", null: false
     t.bigint "location_id", null: false
     t.index ["location_id"], name: "index_posts_on_location_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
