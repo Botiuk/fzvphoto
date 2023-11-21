63dd1ded8bfcd0fbad95bf8e6b56c5a4d0dfce5f
botiuk <svetabotiuk@gmail.com>
2023-11-21 12:13:30 +0200

    add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   resources :locations
   resources :teams
   resources :tournaments
   resources :stadia
   resources :matches
-  root "locations#index"
+  resources :posts
+  
+  root "posts#index"
 end
