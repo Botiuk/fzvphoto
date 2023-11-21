e0c3a0d923928a583e312800368a677f0e0093cc
botiuk <svetabotiuk@gmail.com>
2023-11-21 11:59:16 +0200

    create posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+    @locations = Location.formhelper
+  end
+
+  def edit
+    @locations = Location.formhelper
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @locations = Location.formhelper
+    if @post.save
+      redirect_to posts_url, notice: t('notice.create.post')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_url, notice: t('notice.update.post')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: t('notice.destroy.post')
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :location_id)
+  end
+
+  def authenticate_user!
+    redirect_to posts_url unless user_signed_in?
+  end
+end
