1d14fa8491333926f01e5988e85f5b27315229c2
botiuk <svetabotiuk@gmail.com>
2023-11-29 22:00:40 +0200

    add order to posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:postdate, :id).reverse_order
   end
 
   def show
