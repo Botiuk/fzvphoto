228e4d825dd50da08a5a470abc2ff181eca7dd00
botiuk <svetabotiuk@gmail.com>
2024-01-24 12:47:50 +0200

    destroy model StadiumPosts

--- app/models/stadium_post.rb
+++
@@ -1,4 +0,0 @@
-class StadiumPost < ApplicationRecord
-  belongs_to :stadium
-  belongs_to :post
-end
--- test/models/stadium_post_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class StadiumPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
