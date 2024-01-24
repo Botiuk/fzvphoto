79050d92d44e6dce84f3e24189e96de883f83026
botiuk <svetabotiuk@gmail.com>
2024-01-24 12:45:56 +0200

    clean routes and other views from stadium_posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,11 @@ Rails.application.routes.draw do
   get 'teams/searchposts', to: 'teams#searchposts'
   get 'tournaments/searchposts', to: 'tournaments#searchposts'
   get 'stadia/searchposts', to: 'stadia#searchposts'
-  get 'stadium_posts/searchposts', to: 'stadium_posts#searchposts'
 
   get 'locations/searchalbums', to: 'locations#searchalbums'
   get 'teams/searchalbums', to: 'teams#searchalbums'
   get 'tournaments/searchalbums', to: 'tournaments#searchalbums'
-  get 'stadia/searchalbums', to: 'stadia#searchalbums'  
-  get 'stadium_posts/searchalbums', to: 'stadium_posts#searchalbums'
+  get 'stadia/searchalbums', to: 'stadia#searchalbums'
   
   get 'posts/typeposts', to: 'posts#typeposts'
 
