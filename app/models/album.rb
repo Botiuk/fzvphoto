e41765c4c2ae45625ab4527330633de93dbff913
botiuk <svetabotiuk@gmail.com>
2024-02-08 12:53:54 +0200

    add association has one match through post to album, simplify methods

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :post
+  has_one :match, through: :post
   has_many_attached :photos
 
   def self.formhelper_posts
@@ -19,13 +20,11 @@ class Album < ApplicationRecord
   end
 
   def self.search_team(team_id)
-    posts_id = Post.search_team(team_id).pluck(:id)
-    Album.search_by_post(posts_id)
+    Album.joins(:match).where(match: {home_team_id: team_id}).or(Album.joins(:match).where(match: {visitor_team_id: team_id})).order(:id).reverse_order
   end
 
   def self.search_tournament(tournament_id)
-    posts_id = Post.search_tournament(tournament_id).pluck(:id)
-    Album.search_by_post(posts_id)
+    Album.joins(:match).where(match: {tournament_id: tournament_id}).order(:id).reverse_order
   end
 
   def self.search_stadium(stadium_id)
