597c429a3b1e1353398a45ed9e5c5c59f372fcab
botiuk <svetabotiuk@gmail.com>
2024-11-20 13:11:37 +0200

    changed drop migration to reversible

--- db/migrate/20240124103102_drop_stadium_posts.rb
+++ db/migrate/20240124103102_drop_stadium_posts.rb
@@ -2,6 +2,11 @@
 
 class DropStadiumPosts < ActiveRecord::Migration[7.0]
   def change
-    drop_table :stadium_posts
+    drop_table :stadium_posts do |t|
+      t.references :stadium, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
